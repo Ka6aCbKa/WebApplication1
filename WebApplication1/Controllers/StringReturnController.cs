b5dd040c59764d97dbfac0fd19e4ba4f6d0871f1
Ka6acbka Gex <map4ehok@mail.ru>
2021-02-18 12:28:30 +0200

    Random string return

---
+++ WebApplication1/Controllers/StringReturnController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StringReturnController : ControllerBase
+    {
+        //пока хз зачем это
+        private readonly ILogger<StringReturnController> _logger;
+        //пока хз зачем это
+        public StringReturnController(ILogger<StringReturnController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public String Get()
+        {
+            return StringReturn.GetRandomString();
+        }
+
+    }
+}
---
+++ WebApplication1/StringReturn.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class StringReturn
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public static string GetRandomString()
+        {
+            var rng = new Random();
+            return Summaries[rng.Next(Summaries.Length)];
+        }
+    }
+}
