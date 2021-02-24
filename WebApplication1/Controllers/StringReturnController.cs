a8a3305311ded55daa97ffdb31f9c3517c12b41b
Ka6acbka Gex <map4ehok@mail.ru>
2021-02-24 23:24:55 +0200

    Request path added to logger

--- WebApplication1/Controllers/StringReturnController.cs
+++ WebApplication1/Controllers/StringReturnController.cs
@@ -20,7 +20,7 @@ namespace WebApplication1.Controllers
         [HttpGet]
         public String Get(int limit = 5)
         {
-            _logger.LogInformation($"{DateTime.Now}: Got {limit} elements");
+            _logger.LogInformation($"{DateTime.Now}: Got {limit} elements ("+ this.HttpContext.Request.Method + " " + this.HttpContext.Request.Path + this.HttpContext.Request.QueryString + ")");
             return StringReturn.GetLast(limit);
         }
 
@@ -28,7 +28,7 @@ namespace WebApplication1.Controllers
         public String AddString(Product prod)
         {
             StringReturn.AddString(prod.Name);
-            _logger.LogInformation($"{DateTime.Now}: Added \"{prod.Name}\"");
+            _logger.LogInformation($"{DateTime.Now}: Added \"{prod.Name}\" (" + this.HttpContext.Request.Method + " " + this.HttpContext.Request.Path + ")");
             return StringReturn.GetLast(1);
         }
     }
