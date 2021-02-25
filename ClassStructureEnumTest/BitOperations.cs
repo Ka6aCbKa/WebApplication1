66343578b7fa5e445f34c078f05512cb09ba412f
Ka6acbka Gex <map4ehok@mail.ru>
2021-02-25 23:14:06 +0200

    Enum bitmap test

---
+++ ClassStructureEnumTest/BitOperations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ClassStructureEnumTest
+{
+    [Flags]
+    public enum Bitmap : uint
+    {
+        none = 0,
+        first = 1,
+        second = 2,
+        third = 4,
+        forth = 8,
+        fifth = 16,
+        sixth = 32,
+        seventh = 64,
+        eigth = 128,
+        all = ~none
+    }
+    class BitOperations
+    {
+        public Bitmap Value {get; private set;}
+
+        public BitOperations() => this.Value = Bitmap.none;
+        public BitOperations(Bitmap value) => this.Value = value;
+        public void Add(Bitmap value) => this.Value |= value;
+        public void Remove(Bitmap value) => this.Value ^= value;
+        public bool Contains(Bitmap value) => (this.Value & value) == value;
+        public override string ToString() => this.Value.ToString();
+    }
+}
---
+++ ClassStructureEnumTest/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ClassStructureEnumTest
+{    
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+
+
+            //Bitmap test
+            {
+                Bitmap arg1 = Bitmap.fifth | Bitmap.first;
+
+                Console.WriteLine(arg1.ToString());
+
+                arg1 ^= Bitmap.fifth;
+                Console.WriteLine(arg1.ToString());
+
+                arg1 |= Bitmap.forth | Bitmap.sixth;
+                Console.WriteLine(arg1.ToString());
+
+                bool y = (arg1 & Bitmap.sixth) == Bitmap.sixth;
+                Console.WriteLine(y);
+
+                y = arg1.HasFlag(Bitmap.eigth);
+                Console.WriteLine(y);
+                Console.WriteLine();
+
+
+                BitOperations arg2 = new BitOperations(Bitmap.sixth);
+                Console.WriteLine(arg2);
+
+                arg2.Add(Bitmap.seventh | Bitmap.second);
+                Console.WriteLine(arg2);
+
+                if (arg2.Contains(Bitmap.second))
+                {
+                    arg2.Remove(Bitmap.second);
+                }
+                Console.WriteLine(arg2);
+
+                y = arg2.Contains(Bitmap.second);
+                Console.WriteLine(y);
+            }
+            Console.ReadLine();
+        }
+    }
+}
